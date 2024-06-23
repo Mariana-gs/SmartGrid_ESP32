3d331c71cc2b253555060fe716a8e50e321b9673
Mariana Soares <81964220+Mariana-gs@users.noreply.github.com>
2024-06-23 19:19:43 -0300

    reduzindo tempo de resposta

--- main/main.ino
+++ main/main.ino
@@ -1,39 +1,24 @@
-// Bibliotecas
 #include <WiFi.h>
-//#include <FirebaseESP32.h>
 #include <Adafruit_NeoPixel.h>
-// #include <SinricPro.h>
-// #include <SinricProSwitch.h>
 #include <IOXhop_FirebaseESP32.h>
 #include <ArduinoJson.h>
 
 // Wifi
-#define WIFI_SSID ""        // Nome da rede
-#define WIFI_PASSWORD ""    // Senha da rede
+#define WIFI_SSID "SOARES"
+#define WIFI_PASSWORD "31060810#*"
 
 // Firebase
-#define FIREBASE_HOST "https://drawer-57cde-default-rtdb.firebaseio.com/"      // Url do projeto 
-#define FIREBASE_AUTH "X9Io7QZYS7HudcHYIsxKe7xauAAqbwl5dNGNmzxb"      // Token de autenticacao 
+#define FIREBASE_HOST "https://drawer-57cde-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "X9Io7QZYS7HudcHYIsxKe7xauAAqbwl5dNGNmzxb"
 
 // LED
-#define LED_PIN 2           // Pino do EPS32 
-#define NUM_LEDS 29         // Numero de LEDs na fita
+#define LED_PIN 13
+#define NUM_LEDS 29
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// SinricPro
-#define APP_KEY "00994d24-7469-4941-a075-8e14e059060f"                                             // Credencial 
-#define APP_SECRET "232b62e8-f588-46d8-aeb2-1239c96e679c-1a6f0934-260a-45ea-8131-17c97bc18b1d"     // Credencial 
-#define SWITCH_ID "665b80d55d818a66fab35577"                                                       // Identificador do dispositivo 
-
-// Objeto firebase
-// FirebaseData firebaseData;
-// FirebaseConfig firebaseConfig;   
-// FirebaseAuth firebaseAuth; 
-
 // Mapeamento de gavetas para os LEDs
-// Cada linha -> uma gaveta, cada coluna -> um LED associado a ela
 const int drawerLEDMap[9][4] = {
-  {0, 1, 2, -1},    
+  {0, 1, 2, -1},
   {3, 4, 5, -1},
   {6, 7, 8, -1},
   {9, 10, 11, -1},
@@ -44,83 +29,59 @@ const int drawerLEDMap[9][4] = {
   {26, 27, 28, -1}
 };
 
-// Variavel de controle do efeito visual basico
 bool visualEffect = true;
 
 void setup() {
   Serial.begin(115200);
 
-  // Conexão com Wi-Fi
-  
   connectToWiFi();
 
-  // Inicializacao dos LEDs
   strip.begin();
   strip.show();
 
-
-  // Inicializacao do Firebase
-  // firebaseConfig.host = FIREBASE_HOST;
-  // firebaseConfig.api_key = FIREBASE_AUTH;
-  // firebaseConfig.signer.tokens.legacy_token = FIREBASE_AUTH;
-
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  // Firebase.reconnectWiFi(true);
-
-  // Inicializacao do SinricPro
-  /*SinricProSwitch &mySwitch = SinricPro[SWITCH_ID];
-  mySwitch.onPowerState(onPowerState);
-  SinricPro.begin(APP_KEY, APP_SECRET);*/
-
-  
 }
 
 void loop() {
-  //SinricPro.handle();
-
-  // Verifica a conexao Wi-Fi e reconecta se necessario
-   /*if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED) {
     connectToWiFi();
-  }*/
-
-  // Le o estado do Firebase para obter o ID da gaveta acionada
-  if(Firebase.getInt("/drawer/config/id_drawer")) {
-     int drawerId = Firebase.getInt("/drawer/config/id_drawer");
-
-    if (Firebase.failed()) {
-      Serial.println("Firebase get failed");
-      Serial.println(Firebase.error());
-      return;
   }
 
-    if(drawerId > 0 && drawerId <= 9) {
-      lightUpDrawer(drawerId-1);
-      visualEffect = false;
-    }
-    else {
+  int drawerId = Firebase.getInt("/drawer/config/id_drawer");
+
+  if (Firebase.failed()) {
+    Serial.println("Falha ao ler o ID da gaveta no Firebase");
+    visualEffect = true;
+  } else {
+    if (drawerId > 0 && drawerId <= 9) {
+      int r = Firebase.getInt("/drawer/config/drawer_color/R");
+      int g = Firebase.getInt("/drawer/config/drawer_color/G");
+      int b = Firebase.getInt("/drawer/config/drawer_color/B");
+
+      if (Firebase.failed()) {
+        Serial.println("Falha ao ler as cores da gaveta no Firebase");
+        visualEffect = true;
+      } else {
+        lightUpDrawer(drawerId - 1, r, g, b);
+        visualEffect = false;
+      }
+    } else {
       visualEffect = true;
     }
   }
-  
-  else {
-    Serial.println("Falha ao ler os dados do Firebase");
-    visualEffect = true;
-  }
 
-  if(visualEffect) {
+  if (visualEffect) {
     showVisualEffect();
   }
 
-  delay(1000);
+  delay(100);
 }
 
-// Conecta Wi-Fi
 void connectToWiFi() {
   Serial.println("Conectando ao Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  
+
   int attempts = 0;
-  
   while (WiFi.status() != WL_CONNECTED && attempts < 20) {
     delay(500);
     Serial.print(".");
@@ -133,40 +94,29 @@ void connectToWiFi() {
     Serial.println("Falha ao conectar ao Wi-Fi. Tentando novamente em 5 segundos.");
     delay(5000);
   }
-} 
+}
 
-// Efeito visual 
 void showVisualEffect() {
   int R = random(0, 255);
   int G = random(0, 255);
   int B = random(0, 255);
 
-  for(int i = 0; i < NUM_LEDS; i++) {
+  for (int i = 0; i < NUM_LEDS; i++) {
     strip.setPixelColor(i, strip.Color(R, G, B));
   }
-
   strip.show();
 }
 
-// Ascende determinada gaveta
-void lightUpDrawer(int drawerId) {
-  for(int i = 0; i < NUM_LEDS; i++) {
+void lightUpDrawer(int drawerId, int r, int g, int b) {
+  for (int i = 0; i < NUM_LEDS; i++) {
     strip.setPixelColor(i, strip.Color(0, 0, 0));
   }
 
-  for(int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++) {
     int ledIndex = drawerLEDMap[drawerId][i];
-
-    if(ledIndex != -1) {
-      strip.setPixelColor(ledIndex, strip.Color(Firebase.getInt("/drawer/config/drawer_color/R"), Firebase.getInt("/drawer/config/drawer_color/G"), Firebase.getInt("/drawer/config/drawer_color/B")));    // Cor pode ser modificada com o valor da tabela
+    if (ledIndex != -1) {
+      strip.setPixelColor(ledIndex, strip.Color(r, g, b));
     }
   }
   strip.show();
 }
-
-// Callback - mudança do estado de energia do dispositivo 
-// bool onPowerState(const String &deviceId, bool &state) {
- //  Serial.printf("Dispositivo %s ligado %s\r\n", deviceId.c_str(), state ? "on" : "off");
-  // return true;
-// }
-
